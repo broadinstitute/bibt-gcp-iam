d1c01083a06d756f744a9af425e7fa6079e41443
Matthew OBrien <mobrien@broadinstitute.org>
2023-12-11 17:09:03 -0500

    metadata/config upgrades

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,11 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.insert(0, os.path.abspath("."))
 
-from bibt.template import __version__  # noqa: E402
+from bibt.gcp.iam import __version__  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
-project = "bibt-template"
+project = "bibt-gcp-iam"
 copyright = "2023, Matthew OBrien"
 author = "Matthew OBrien"
 version = __version__
